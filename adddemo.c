59647a42386a830d886546e4c46e869a4e4cfc41
devabhargav <39043413+devabhargav@users.noreply.github.com>
2018-10-05 12:22:56 +0530

    Create adddemo.c

---
+++ adddemo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum=0;
+printf("enter number a\n");
+scanf("%d",&a);
+printf("enter number b\n");
+scanf("%d",&b);
+sum=add(a,b);
+printf("sum is %d",sum);
+}
+int add(int a,int b)
+{
+sum=a+b;
+return 0;
+}
